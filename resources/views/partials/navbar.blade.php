5278004390a911d9bcbdfb830362780eb9b175b9
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-10-01 22:19:09 +0800

    hiding create on create route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,6 @@
 <div class="navigation mb-5">
     <a class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded" href="{{route('product.index')}}">Home</a>
-    <a class="bg-green-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded" href="{{route('product.create')}}">Create</a>
+    @unless(Route::is('product.create'))
+        <a class="bg-green-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded" href="{{route('product.create')}}">Create</a>
+    @endunless
 </div>
