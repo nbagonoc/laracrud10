b444b4f35a3bb46c985c2abb5ae9490eb24245c7
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-10-02 18:26:10 +0800

    added read unit test

--- tests/Unit/ProductControllerTest.php
+++ tests/Unit/ProductControllerTest.php
@@ -16,7 +16,7 @@ class ProductControllerTest extends TestCase
         Mockery::close();
     }
 
-    public function testIndexMethod()
+    public function testIndex()
     {
         // Create a mock of the Product model to simulate database behavior
         $productMock = Mockery::mock(Product::class);
@@ -29,7 +29,44 @@ class ProductControllerTest extends TestCase
         $response = $controller->index();
 
         // Assertions
-        $this->assertEquals('products.index', $response->getName()); // Assert the view name
+        $this->assertEquals('products.index', $response->name()); // Assert the view name
         $this->assertInstanceOf(Collection::class, $response->getData()['products']); // Assert that 'products' is an instance of Collection
     }
+
+    public function testCreate()
+    {
+        // Create an instance of the ProductController and inject the mock
+        $controller = new ProductController();
+
+        // Call the method under test
+        $response = $controller->create();
+
+        // Assertions
+        $this->assertEquals('products.create', $response->name()); // Assert the view name
+    }
+
+    public function testRead()
+    {
+        $productMock = Mockery::mock(Product::class);
+        $product = new Product([
+            'name' => 'Test Product',
+            'qty' => 10, // Replace with your desired quantity
+            'price' => 19.99, // Replace with your desired price
+            'description' => 'This is a test product description.'
+        ]);
+        $productMock->shouldReceive('findOrFail')->with(1)->andReturn($product); // Mock the 'findOrFail' method
+
+        // Create an instance of the ProductController and inject the mock
+        $controller = new ProductController($productMock);
+
+        // Call the method under test
+        $response = $controller->read($product);
+
+        // Assertions
+        $this->assertEquals('products.read', $response->name()); // Assert the view name
+        $this->assertEquals($product, $response->getData()['product']);
+    }
+
+    // public function test
+
 }
