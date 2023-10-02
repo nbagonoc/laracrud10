717eb23c4e654f73c2caa8cd5e409c899a437192
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-10-02 21:04:56 +0800

    added styling to edit and create

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,22 +4,22 @@
     <form action="{{route('product.save')}}" method="post">
         @csrf
         @method('post')
-        <div>
+        <div class="my-1">
             <label for="name">Name</label>
-            <input  name="name" type="text" placeholder="name">
+            <input name="name" type="text" placeholder="name" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="qty">Qty</label>
-            <input name="qty" type="text" placeholder="qty">
+            <input name="qty" type="text" placeholder="qty" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="price">Price</label>
-            <input name="price" type="text" placeholder="price">
+            <input name="price" type="text" placeholder="price" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="description">Description</label>
-            <input name="description" type="text" placeholder="description">
+            <input name="description" type="text" placeholder="description" class="border rounded p-3 py-1">
         </div>
-        <input type="submit" value="Save a new product" class="bg-green-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded"">
+        <input type="submit" value="Save Product" class="bg-green-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded mt-2">
     </form>
 @endsection
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -4,22 +4,22 @@
     <form action="{{route('product.update',$product)}}" method="post">
         @csrf
         @method('put')
-        <div>
+        <div class="my-1">
             <label for="name">Name</label>
-            <input  name="name" type="text" placeholder="name" value="{{$product->name}}">
+            <input  name="name" type="text" placeholder="name" value="{{$product->name}}" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="qty">Qty</label>
-            <input name="qty" type="text" placeholder="qty" value="{{$product->qty}}">
+            <input name="qty" type="text" placeholder="qty" value="{{$product->qty}}" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="price">Price</label>
-            <input name="price" type="text" placeholder="price" value="{{$product->price}}">
+            <input name="price" type="text" placeholder="price" value="{{$product->price}}" class="border rounded p-3 py-1">
         </div>
-        <div>
+        <div class="my-1">
             <label for="description">Description</label>
-            <input name="description" type="text" placeholder="description" value="{{$product->description}}">
+            <input name="description" type="text" placeholder="description" value="{{$product->description}}" class="border rounded p-3 py-1">
         </div>
-        <input type="submit" value="Update">
+        <input type="submit" value="Update Product" class="bg-green-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded mt-2">
     </form>
 @endsection
